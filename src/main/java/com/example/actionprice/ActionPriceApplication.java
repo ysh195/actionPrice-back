88d0149609612b53b47aaab84e9d4888d5661b69
ysh195 <bscom195@gmail.com>
2024-09-19 15:34:40 +0900

    first commit

---
+++ src/main/java/com/example/actionprice/ActionPriceApplication.java
@@ -0,0 +1,13 @@
+package com.example.actionprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionPriceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ActionPriceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/actionprice/ActionPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actionprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionPriceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
