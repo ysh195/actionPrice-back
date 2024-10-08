2040d7be38f7922f6b31f779156efd6bde9cbbe1
home-in-00 <lhm3052@naver.com>
2024-10-08 12:31:18 +0900

    asd

--- src/main/java/com/example/actionprice/originalAuctionData/AuctionDataFetcher.java
+++ src/main/java/com/example/actionprice/originalAuctionData/AuctionDataFetcher.java
@@ -78,7 +78,7 @@ public class AuctionDataFetcher {
 	 * @throws Exception
 	 * @info api의 반환 데이터 구조에 맞춘 OriginAuctionDataBody 객체를 사용하여 비즈니스에 로직에 활용할 수 있도록 구성.
 	 *
-	 * 그리고 웹 클라이언트를 사용함으로써 비동기적인 로직 수행 구현. 
+	 * 그리고 웹 클라이언트를 사용함으로써 비동기적인 로직 수행 구현.
 	 */
 
 
