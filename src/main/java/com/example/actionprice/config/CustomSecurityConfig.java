4af56daad133b267864047e01180e6e62267498c
home-in-00 <lhm3052@naver.com>
2024-10-08 09:56:01 +0900

    테이블 생성 완료

--- src/main/java/com/example/actionprice/config/CustomSecurityConfig.java
+++ src/main/java/com/example/actionprice/config/CustomSecurityConfig.java
@@ -36,8 +36,6 @@ import java.util.Arrays;
  * @updated 24/10/05 20:42
  * @info 어느 정도 개발이 완성되기 전까지는 보안을 포괄적으로 다 열어뒀음. 토큰관련 로직이 추가로 더 필요하긴 함.
  */
-
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
