2f038d47aed6806a11b5f0e35d32d22000c8eae3
ysh195 <bscom195@gmail.com>
2024-10-14 12:30:22 +0900

    241014_연상훈_유저삭제테스트추가

--- src/test/java/com/example/actionprice/user/UserRepositoryTest.java
+++ src/test/java/com/example/actionprice/user/UserRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.example.actionprice.user;
 
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,7 +17,29 @@ public class UserRepositoryTest {
   @Autowired
   private PasswordEncoder passwordEncoder;
 
+  /**
+   * user 삭제하기
+   * @author : 연상훈
+   * @created : 2024-10-14 오후 12:29
+   * @updated : 2024-10-14 오후 12:29
+   * @info : 귀찮더라도 이걸로 삭제해야 정상적으로 삭제됨. DB에서 삭제하는 건 되도록 자제
+   */
   @Test
+  @Disabled
+  public void deleteUserTest(){
+    String username = "ewrqdsq";
+
+    User user = userRepository.findById(username).orElse(null);
+    if(user != null){
+      System.out.println("the user exists. and we delete the user");
+      userRepository.delete(user);
+      return;
+    }
+    System.out.println("the user doesn't exists. and we can't delete the user");
+  }
+
+  @Test
+  @Disabled
   public void userCreateTest() {
     User user = User.builder()
         .username("useraaaa")
