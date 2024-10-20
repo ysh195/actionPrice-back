68a86da467e5517c33a6c4a769d884dccc544956
ysh195 <bscom195@gmail.com>
2024-10-20 19:09:48 +0900

    241020_연상훈_유효성검사_추가

--- src/main/java/com/example/actionprice/user/forms/UserRegisterForm.java
+++ src/main/java/com/example/actionprice/user/forms/UserRegisterForm.java
@@ -3,7 +3,6 @@ package com.example.actionprice.user.forms;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +11,10 @@ import lombok.NoArgsConstructor;
  * @author 연상훈
  * @group CheckForDuplicateUsernameGroup : username 중복 체크 검증그룹
  * @group SendVerificationCodeGroup : 인증코드 검증그룹
+ * @group CheckValidityOfPasswordGroup : password 정규식 유효성 검증그룹
  * @created 2024-10-05 오후 10:57
  * @updated 2024-10-12 오후 1:48 : 전체 검증은 그냥 @Valid를 쓰기 때문에 불필요한 검증 그룹인 CheckVerificationCodeGroup 삭제
- * @updated 2024-10-20 오전 10:54 : 패스워드만 검증하기 위한 그룹 CheckValidityOfPasswordGroup 추가. 패스워드의 기존 검증을 정규식 패턴 검증으로 대체
+ * @updated 2024-10-20 오전 10:54 : 1. 패스워드만 검증하기 위한 그룹 CheckValidityOfPasswordGroup 추가. 2. username과 password에 정규식을 이용한 유효성 검증 추가
  * @info 사용자가 user로 등록할 때 입력한 데이터를 담아올 객체입니다. 실질적으로 이게 dto 역할입니다.
  * @info 하나의 form이지만, 상황에 따라 검증이 필요한 영역이 다르기 때문에 검증을 위한 별도의 그룹을 지정하여 처리합니다.
  */
@@ -23,17 +23,22 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserRegisterForm {
 
+  // 현재 정규식 : 6~20자, 영어+숫자
   @NotBlank(
-      message = "사용자 이름은 필수입니다.",
+      message = "사용자 이름은 필수 입력사항입니다.",
       groups = {CheckForDuplicateUsernameGroup.class, SendVerificationCodeGroup.class})
-  @Size(
-      min = 6, max = 20, message = "사용자 이름은 6자 이상 20자 이하여야 합니다.",
+  @Pattern(
+      regexp = "^[a-zA-Z0-9]{6,20}$",
+      message = "사용자 이름은 6~20자의 영어와 숫자로 구성됩니다.",
       groups = {CheckForDuplicateUsernameGroup.class, SendVerificationCodeGroup.class})
   private String username;
 
-  // 현재 정규식 : 8~20자, 영어+숫자+특수문자
+  // 현재 정규식 : 8~16자, 영어+숫자+특수문자
+  @NotBlank(
+      message = "비밀번호는 필수 입력사항입니다.",
+      groups = {SendVerificationCodeGroup.class, CheckValidityOfPasswordGroup.class})
   @Pattern(
-      regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,20}$",
+      regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,16}$",
       message = "비밀번호는 8~16자로 구성되며, 영어, 숫자, 특수문자를 각각 하나 이상 포함해야 합니다.",
       groups = {SendVerificationCodeGroup.class, CheckValidityOfPasswordGroup.class})
   private String password;
