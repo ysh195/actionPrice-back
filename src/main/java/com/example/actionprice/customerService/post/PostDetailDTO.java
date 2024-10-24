956260623ba97675119a864d46a09f0df1eb7247
ysh195 <ysh195@gmail.com>
2024-10-24 16:16:43 +0900

    241024_연상훈_post관련5

--- src/main/java/com/example/actionprice/customerService/post/PostDetailDTO.java
+++ src/main/java/com/example/actionprice/customerService/post/PostDetailDTO.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 @Setter
 @Builder
-@Getter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostDetailDTO {
--- src/test/java/com/example/actionprice/post/PostTest.java
+++ src/test/java/com/example/actionprice/post/PostTest.java
@@ -1,7 +1,9 @@
 package com.example.actionprice.post;
 
+import com.example.actionprice.customerService.post.PostController;
 import com.example.actionprice.customerService.post.PostService;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,6 +13,14 @@ public class PostTest {
 
     @Autowired
     PostService postService;
+
+    @Autowired
+    PostController postController;
+
+    @Test
+    public void test() {
+        System.out.println(postController.goDetailPost(1).toString());
+    }
 
 
 }
