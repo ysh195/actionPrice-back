8f61018499ca95ae2df38647a6f2403bf5ce8e47
home-in-00 <lhm3052@naver.com>
2024-11-05 01:16:14 +0900

    241104 최적화

--- src/test/java/com/example/actionprice/Excel/ExcelServiceTest.java
+++ src/test/java/com/example/actionprice/Excel/ExcelServiceTest.java
@@ -22,29 +22,29 @@ public class ExcelServiceTest {
     @Autowired
     AuctionCategoryService auctionCategoryService;
 
-
-    // 엑셀 응답 테스트
-    @Disabled
-    @Test
-    void createExcelFile_ShouldReturnExcelFile() {
-        List<AuctionBaseEntity> transactionHistoryList = new ArrayList<>();
-
-        // Builder 패턴을 사용하여 객체 생성
-        transactionHistoryList.add(AuctionBaseEntity.builder()
-                .delDate(LocalDate.of(2024, 10, 1))
-                .large("축산물")
-                .middle("돼지")
-                .productName("삼겹살")
-                .productRank("1등급")
-                .del_unit("kg")
-                .price(15000)
-                .build());
-
-        // 엑셀 파일 생성 메서드 호출
-        ResponseEntity<byte[]> responseEntity = auctionCategoryService.createExcelFile(transactionHistoryList);
-        // 응답 상태 및 바이트 배열 확인
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertNotNull(responseEntity.getBody());
-    }
+//
+//    // 엑셀 응답 테스트
+//    @Disabled
+//    @Test
+//    void createExcelFile_ShouldReturnExcelFile() {
+//        List<AuctionBaseEntity> transactionHistoryList = new ArrayList<>();
+//
+//        // Builder 패턴을 사용하여 객체 생성
+//        transactionHistoryList.add(AuctionBaseEntity.builder()
+//                .delDate(LocalDate.of(2024, 10, 1))
+//                .large("축산물")
+//                .middle("돼지")
+//                .productName("삼겹살")
+//                .productRank("1등급")
+//                .del_unit("kg")
+//                .price(15000)
+//                .build());
+//
+//        // 엑셀 파일 생성 메서드 호출
+//        ResponseEntity<byte[]> responseEntity = auctionCategoryService.createExcelFile(transactionHistoryList);
+//        // 응답 상태 및 바이트 배열 확인
+//        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+//        assertNotNull(responseEntity.getBody());
+//    }
 }
 
