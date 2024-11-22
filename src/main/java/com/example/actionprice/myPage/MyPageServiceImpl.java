786f07a6d344defb86eaba377ebf8cbaec91affb
ysh195 <bscom195@gmail.com>
2024-11-23 03:26:16 +0900

    241123_연상훈_정지유저_탈퇴_막음

--- src/main/java/com/example/actionprice/myPage/MyPageServiceImpl.java
+++ src/main/java/com/example/actionprice/myPage/MyPageServiceImpl.java
@@ -2,12 +2,16 @@ package com.example.actionprice.myPage;
 
 import com.example.actionprice.customerService.post.PostService;
 import com.example.actionprice.customerService.post.dto.PostListDTO;
+import com.example.actionprice.exception.RefreshTokenException;
+import com.example.actionprice.exception.TokenErrors;
 import com.example.actionprice.exception.UserNotFoundException;
+import com.example.actionprice.security.jwt.refreshToken.RefreshTokenEntity;
 import com.example.actionprice.user.User;
 import com.example.actionprice.user.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -58,6 +62,13 @@ public class MyPageServiceImpl implements MyPageService {
         log.info("[class] MyPageServiceImpl - [method] deleteUser > 실행");
         User user = userRepository.findById(username)
             .orElseThrow(() -> new UserNotFoundException("user(" + username + ") does not exist"));
+
+        RefreshTokenEntity refreshTokenEntity = user.getRefreshToken();
+        if (refreshTokenEntity != null) {
+            if (refreshTokenEntity.isBlocked()){
+                throw new AccessDeniedException("your account is blocked");
+            }
+        }
 
         userRepository.delete(user);
     }
