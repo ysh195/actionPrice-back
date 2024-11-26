6d7a4ef28695a8a44e693e6a51cc72a71cce0520
ysh195 <bscom195@gmail.com>
2024-11-26 18:23:51 +0900

    241126_연상훈_테스트_정리

--- src/test/java/com/example/actionprice/ActionPriceApplicationTests.java
+++ src/test/java/com/example/actionprice/ActionPriceApplicationTests.java
@@ -1,11 +1,6 @@
 package com.example.actionprice;
 
-import com.example.actionprice.home.HomeService;
-import java.io.IOException;
-import java.util.Map;
 import lombok.extern.log4j.Log4j2;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
@@ -15,18 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @updated 2024-10-17 오후 7:51
  * @info : 불필요한 테스트는 바로 삭제
  */
-// TODO 데이터 저장 기능은 논의 후 구현
+
 @SpringBootTest(classes = {ActionPriceApplication.class})
 @Log4j2
 class ActionPriceApplicationTests {
-
-  @Autowired
-  HomeService homeService;
-
-  @Test
-  public void contextLoads() throws IOException {
-    Map<String, String> map = homeService.fetchImages();
-    System.out.println(map.get("cookie"));
-  }
 
 }
